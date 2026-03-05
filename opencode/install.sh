f92a99b0bd31f3fe3d82634b680d53ff4d5d153e
Dennis Evert <dennisevert@gmail.com>
2026-03-04 22:03:29 -0600

    WIP setting up opencode

---
+++ opencode/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+# Symlink settings file
+mkdir -p ~/.config/opencode
+ln -sfn ~/.dotfiles/opencode/opencode.jsonc ~/.config/opencode/opencode.jsonc
+ln -sfn ~/.dotfiles/opencode/tui.jsonc ~/.config/opencode/tui.jsonc
