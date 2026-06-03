8dd67d8305a5ed06c6d7d4e558dde3922210048e
Dennis Evert <dennisevert@gmail.com>
2026-06-02 21:44:31 -0500

    Yazi config for heic file icon and additional dep installs for pdf, font and image previewing in Yazi

--- yazi/install.sh
+++ yazi/install.sh
@@ -3,6 +3,10 @@
 echo "Installing Yazi..."
 brew install yazi
 
+echo "Installing Yazi deps..."
+brew install poppler
+brew install imagemagick
+
 echo "Installing Yazi plugins..."
 sudo ya pkg add yazi-rs/plugins:smart-enter
 
@@ -10,5 +14,6 @@ echo "Configuring Yazi..."
 sudo mkdir -p $HOME/.config/yazi
 sudo ln -sfn $HOME/.dotfiles/yazi/yazi.toml $HOME/.config/yazi/yazi.toml
 sudo ln -sfn $HOME/.dotfiles/yazi/keymap.toml $HOME/.config/yazi/keymap.toml
+sudo ln -sfn $HOME/.dotfiles/yazi/theme.toml $HOME/.config/yazi/theme.toml
 
 echo "Yazi install and configuration complete!"
