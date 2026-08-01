f5955e7428073ba730ec8526844e2013d1c0b122
Dennis Evert <dennisevert@gmail.com>
2026-08-01 16:04:11 -0500

    Remove sudo from symlink requests in neovim install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Symlink neovim config files
-sudo ln -s ~/.dotfiles/neovim/init.lua ~/.config/nvim/init.lua
-sudo ln -s ~/.dotfiles/neovim/lazy-lock.json ~/.config/nvim/lazy-lock.json
-sudo ln -s ~/.dotfiles/neovim/lua ~/.config/nvim/lua
+ln -sfn ~/.dotfiles/neovim/init.lua ~/.config/nvim/init.lua
+ln -sfn ~/.dotfiles/neovim/lazy-lock.json ~/.config/nvim/lazy-lock.json
+ln -sfn ~/.dotfiles/neovim/lua ~/.config/nvim/lua
