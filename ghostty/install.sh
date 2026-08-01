f67448ab3c30a499c03547149f0098562e2349d4
Dennis Evert <dennisevert@gmail.com>
2026-08-01 15:54:33 -0500

    Ghostty install script tweak to remove sudo

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -6,7 +6,7 @@ echo "Configuring Ghostty..."
 # Ensure config directory exists
 mkdir -p ~/.config/ghostty
 
-sudo ln -sfn ~/.dotfiles/ghostty/config ~/.config/ghostty/config
+ln -sfn ~/.dotfiles/ghostty/config ~/.config/ghostty/config
 
 echo "Ghostty Configuration Complete"
 echo "Reload Configuration with: cmd+shift+,"
