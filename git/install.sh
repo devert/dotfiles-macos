87bec1104787cda3dedd55fcc7b9fcd98681825a
Dennis Evert <dennisevert@gmail.com>
2026-08-01 16:00:47 -0500

    Git config and install script tweaks

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-# Install Git and GitHub CLI 
+# Install Git and GitHub CLI
 echo "Installing Git and GitHub CLI..."
 
 brew install git
@@ -9,14 +9,14 @@ brew install gh
 # Symlink .gitconfig and .gitignore_global
 echo "Installing Git configuration files..."
 
-sudo ln -sfn ~/.dotfiles/git/.gitconfig ~/.gitconfig
-sudo ln -sfn ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -sfn ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -sfn ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 
 # Configuration settings for Github CLI
 echo "Installing GitHub CLI configuration files..."
 
 mkdir ~/.config/gh
-sudo ln -s ~/.dotfiles/git/gh_config.yml ~/.config/gh/config.yml
+ln -s ~/.dotfiles/git/gh_config.yml ~/.config/gh/config.yml
 
 # Install some GitHub CLI extensions
 echo "Installing GitHub CLI extensions..."
